7fc89786db7166d87b34986ff1426b0f231049ed
amanefk <158782549+amanefk@users.noreply.github.com>
2025-07-08 16:10:07 +0200

    Update accueil.php

--- projetJoan/view/accueil.php
+++ projetJoan/view/accueil.php
@@ -1,12 +1,12 @@
 <main class="accueil">
     <section class="intro">
-        <h2>Bienvenue au Restaurant Le Délice</h2>
-        <p>Chez nous, chaque assiette raconte une histoire… et chaque visite mérite une trace. Nous avons créé ce Livre d’Or pour que vous puissiez y déposer un mot, une impression, un souvenir.</p>
+        <h2>Bienvenue au Restaurant Fukoeka</h2>
+        <p>私たちの料理には一皿ごとに物語があります… そして、訪れてくださったすべての方の思い出を残す価値があります。 この「ゲストブック」は、皆さまの言葉や印象、思い出を書き留めていただくためにご用意しました。Chez nous, chaque assiette raconte une histoire… et chaque visite mérite une trace. Nous avons créé ce Livre d’Or pour que vous puissiez y déposer un mot, une impression, un souvenir.</p>
         <p>Installez-vous, laissez parler votre cœur et vos papilles.</p>
         <a href="index.php?page=livreDor" class="cta">🖊️ Signer le Livre d’Or</a>
     </section>
     <section class="ambiance">
     <img src="public/img/images.jpeg" class="photo">
-        <figcaption>Une ambiance chaleureuse et gourmande…</figcaption>
+        <figcaption>Une ambiance ancestrale et humaine</figcaption>
     </section>
 </main>
